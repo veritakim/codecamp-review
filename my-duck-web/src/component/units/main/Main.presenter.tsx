26131f79c8bf78a000693bda6255a1f639202e7c
veritakim <kjs3597@gmail.com>
2022-05-18 14:15:48 +0900

    calendar ing

--- my-duck-web/src/component/units/main/Main.presenter.tsx
+++ my-duck-web/src/component/units/main/Main.presenter.tsx
@@ -1,5 +1,7 @@
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css'; 
+import moment from 'moment';
 
 interface CalendarProps {
   value: Date
@@ -7,11 +9,14 @@ interface CalendarProps {
 }
 
 export default function MainUi (props: CalendarProps) {
-
+  const [value, onChange] = useState(new Date());
 
   return (
     <div>
-      <Calendar onChange={props.onChange} value={props.value} />
+      <Calendar 
+        onChange={props.onChange} 
+        value={props.value} 
+        />
     </div>
   )
 }
